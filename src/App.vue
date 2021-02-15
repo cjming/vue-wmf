d5969b044034b83bd5adab42fcd3ca9e5c144353
Clare Ming <clareming@gmail.com>
2021-02-15 03:07:32 -0700

    Add components, styles, service.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <PageList />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PageList from "./components/PageList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    PageList
   }
 };
 </script>
@@ -19,8 +18,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  h1 {
+    text-align: center;
+  }
 }
 </style>
